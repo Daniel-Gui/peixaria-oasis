c927d2acce419fb1d6e2117e043913bd5e025a11
DanielGui <dandanielofc@gmail.com>
2025-03-31 18:43:04 -0300

    sidebar & cart

--- src/lib/Components/Navigation/Dock.svelte
+++ src/lib/Components/Navigation/Dock.svelte
@@ -1,24 +1,24 @@
 <script>
 	import { HomeIcon, MenuIcon, ShoppingBasket } from 'lucide-svelte';
 	import Drawer from './Drawer.svelte';
-	const sidebarId = 'sidebar';
 </script>
 
-<Drawer id={sidebarId}>
-	<div class="dock z-10 bg-gray-100 text-gray-500">
-		<label for={sidebarId}>
-			<MenuIcon class="h-5 w-5" />
-			<span class="dock-label">Menu</span>
-		</label>
+<div class="dock z-10 bg-gray-100 text-gray-500">
+	<label for="sidebar">
+		<MenuIcon class="h-5 w-5" />
+		<span class="dock-label">Menu</span>
+	</label>
 
-		<button class="dock-active">
-			<HomeIcon class="h-5 w-5" />
-			<span class="dock-label">Inicio</span>
-		</button>
+	<button class="dock-active">
+		<HomeIcon class="h-5 w-5" />
+		<span class="dock-label">Inicio</span>
+	</button>
 
-		<button>
-			<ShoppingBasket class="h-5 w-5" />
-			<span class="dock-label">Minha cesta</span>
-		</button>
-	</div>
-</Drawer>
+	<label for="cart">
+		<ShoppingBasket class="h-5 w-5" />
+		<span class="dock-label">Minha cesta</span>
+	</label>
+</div>
+
+<Drawer id="sidebar" direction="start" />
+<Drawer id="cart" direction="end" />
--- src/lib/Components/Navigation/Drawer.svelte
+++ src/lib/Components/Navigation/Drawer.svelte
@@ -1,19 +1,26 @@
-<script>
+<script lang="ts">
 	import { X } from 'lucide-svelte';
+	import { draw } from 'svelte/transition';
 
-	let { children, id = 'my-drawer' } = $props();
+	type DrawerProps = {
+		id: string;
+		direction?: string;
+	};
+
+	let { id = 'my-drawer', direction = 'start' }: DrawerProps = $props();
 </script>
 
-<div class="drawer z-10">
+<div class="drawer z-10" class:drawer-end={direction === 'end'}>
 	<input {id} type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content">
-		<!-- Page content here -->
-		{@render children()}
-	</div>
+
 	<div class="drawer-side">
 		<label for={id} aria-label="close sidebar" class="drawer-overlay"></label>
 		<div class="bg-base-300 text-base-content min-h-full w-80 p-4">
-			<div class="flex justify-end">
+			<div
+				class="flex"
+				class:justify-end={direction === 'start'}
+				class:justify-start={direction === 'end'}
+			>
 				<label for={id} class="btn btn-circle">
 					<X class="h-5 w-5" />
 				</label>
