2e1e5c604109a593486011b6ca39c5faa1ae3bb8
DanielGui <dandanielofc@gmail.com>
2025-04-03 20:00:46 -0300

    Informações do produto

--- src/lib/Components/ProductInfo/ProductInfo.svelte
+++ src/lib/Components/ProductInfo/ProductInfo.svelte
@@ -1,10 +1,17 @@
 <section>
-    <div class="container grid grid-cols-1 md:grid-cols-2">
-        <div>
-            <h1>col 1</h1>
-        </div>
-        <div>
-            <h1>col 2</h1>
-        </div>
-    </div>
+	<div class="container grid grid-cols-1 gap-4 py-10 md:grid-cols-2">
+		<div>
+			<img src="/images/collections/peixes/produto-placeholder.webp" alt="teste" />
+		</div>
+		<div>
+			<h1>Titulo</h1>
+			<p>Preco</p>
+			<div class="flex flex-wrap gap-2">
+				<p>Tags</p>
+			</div>
+			<p>Descricao</p>
+			<button class="btn btn-success w-full">Comprar</button>
+			<p>Você será redirecionado para o whatsapp</p>
+		</div>
+	</div>
 </section>
