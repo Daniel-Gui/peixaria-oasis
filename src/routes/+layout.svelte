19e85530a33f4031a4647a3f0355e96e8ec648d1
DanielGui <dandanielofc@gmail.com>
2025-04-15 14:56:40 -0300

    ajuste no fechamento da sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import Footer from '$lib/Components/Navigation/Footer.svelte';
-import Navbar from '$lib/Components/Navigation/Navbar.svelte';
-import '../app.css';
+	import Navbar from '$lib/Components/Navigation/Navbar.svelte';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
+
 <Navbar />
 {@render children()}
 <Footer />
